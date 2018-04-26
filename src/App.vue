7c14c28e80f5c9799fec9733c94fdca76371d458
小王子 <976228284@qq.com>
2018-04-27 04:03:03 +0800

    homepage

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,255 @@
 <template>
 	<div id="app">
+		<div class="main-body">
+
+        <!-- 顶部导航栏 -->
+        <div class="nav-top layui-bg-green">
+
+            <div class="nav-logo">
+                <img class="nav-logo" src="static/images/logo.png" alt="菜鸟博客" style="margin-left:75px;" pc="" />
+                <img class="nav-logo" src="static/images/logo.png" alt="菜鸟博客" style="margin-left:12px;" mobile="" />
+            </div>
+
+            <ul v-if="login" class="layui-nav layui-bg-green nav-right" lay-filter="">
+
+                <li class="layui-nav-item" pc="">
+                    <a href="javascript:;" @click.prevent="linkMyBlog">
+                        我的博客<span class="layui-badge">9</span>
+                    </a>
+                </li>
+                <li class="layui-nav-item">
+                    <a href="#">
+                        <img v-bind:src="user.icon" class="layui-nav-img"/>
+                        <span pc="">{{user.nickName}}</span>
+                    </a>
+                    <dl class="layui-nav-child">
+                        <dd mobile="">
+                            <a href="javascript:;" @click.prevent="linkMyBlog">我的博客</a>
+                        </dd>
+                        <dd>
+                            <a href="javascript:;" @click.prevent="linkMyCMD">我的控制台</a>
+                        </dd>
+                        <dd>
+                            <a href="javascript:;" @click.prevent="btnUserLogout">退出登录</a>
+                        </dd>
+                    </dl>
+                </li>
+            </ul>
+
+            <ul v-if="!login" class="layui-nav layui-bg-green nav-right" lay-filter="">
+
+                <li class="layui-nav-item" pc="">
+                    <a @click="btnRegist" href="#">注册</a>
+                </li>
+
+                <li class="layui-nav-item" pc="">
+                    <a @click="btnLogin" href="#">登录</a>
+                </li>
+
+                <script type="text/html" id="modal-regist">
+                <br/>
+                <div>
+                    <form class="layui-form">
+                        <div class="layui-form-item">
+                            <label class="layui-form-label">帐号</label>
+                            <div class="layui-input-inline">
+                                <input id="inputRegistAccount" type="text" name="account" required="required" lay-verify="required" placeholder="请输入帐号" autocomplete="off" class="layui-input"/>
+                            </div>
+                            <div class="layui-form-mid layui-word-aux">由字母和数字组成，限定长度6-24</div>
+                        </div>
+                        <div class="layui-form-item">
+                            <label class="layui-form-label">昵称</label>
+                            <div class="layui-input-inline">
+                                <input type="text" name="nickName" placeholder="请输入昵称，默认为账号" autocomplete="off" class="layui-input"/>
+                            </div>
+                            <div class="layui-form-mid layui-word-aux">限定长度1-18</div>
+                        </div>
+                        <div class="layui-form-item">
+                            <label class="layui-form-label">邮箱</label>
+                            <div class="layui-input-inline">
+                                <input type="text" name="email" required="required" lay-verify="required|email" placeholder="请输入邮箱" autocomplete="off" class="layui-input"/>
+                            </div>
+                        </div>
+                        <div class="layui-form-item">
+                            <label class="layui-form-label">密码</label>
+                            <div class="layui-input-inline">
+                                <input type="password" name="password" required="required" lay-verify="required" placeholder="请输入密码" autocomplete="off" class="layui-input"/>
+                            </div>
+                            <div class="layui-form-mid layui-word-aux">限定长度6-24</div>
+                        </div>
+                        <div class="layui-form-item">
+                            <label class="layui-form-label">确认密码</label>
+                            <div class="layui-input-inline">
+                                <input type="password" name="password2" required="required" lay-verify="required" placeholder="再次输入密码" autocomplete="off" class="layui-input"/>
+                            </div>
+                        </div>
+                        <div class="layui-form-item">
+                            <div class="layui-input-block">
+                                <button class="layui-btn" lay-submit="" lay-filter="formUserRegist">确认注册</button>
+                                <button type="reset" class="layui-btn layui-btn-primary">重置</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                </script>
+
+                <script type="text/html" id="modal-login">
+                <br/>
+                <div>
+                    <form class="layui-form">
+                        <div class="layui-form-item">
+                            <label class="layui-form-label">帐号</label>
+                            <div class="layui-input-inline">
+                                <input id="inputLoginAccount" type="text" name="account" required="required" lay-verify="required" placeholder="请输入帐号" autocomplete="off" class="layui-input"/>
+                            </div>
+                        </div>
+                        <div class="layui-form-item">
+                            <label class="layui-form-label">密码</label>
+                            <div class="layui-input-inline">
+                                <input type="password" name="password" required="required" lay-verify="required" placeholder="请输入密码" autocomplete="off" class="layui-input"/>
+                            </div>
+                        </div>
+                        <div class="layui-form-item">
+                            <div class="layui-input-block">
+                                <button class="layui-btn" lay-submit="" lay-filter="formUserLogin">登录</button>
+                                <button type="reset" class="layui-btn layui-btn-primary">重置</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                </script>
+            </ul>
+        </div>
+		</div>
+
 		<router-view/>
 	</div>
 </template>
 
 <script>
 export default {
-	name: 'App'
+	name: 'App',
+	data() {
+		return {
+			login: false,
+			user: {
+				icon: "static/images/default_icon.png",
+				nickName: "游客"
+			}
+		};
+	},
+	mounted() {
+		var Vue = this;
+		layui.use(['element', 'layer', 'form'], function () {
+			Vue.$ = layui.jquery;
+			Vue.layer = layui.layer;
+			Vue.form = layui.form;
+		});
+	},
+	methods: {
+		// 跳转至我的博客
+		linkMyBlog: function(){
+
+		},
+		linkMyCMD: function(){
+
+		},
+		btnUserLogout: function(e){
+			layui.use(['element', 'layer', 'form'], function () {
+				var $ = layui.jquery;
+				var layer = layui.layer;
+				var form = layui.form;
+
+				e.preventDefault();
+
+				var load = layer.load(1);
+				$.ajax({
+					url: "/api/user/logout",
+					type: "PUT",
+					dataType: "json",
+					success: function (data) {
+						layer.close(load);
+						window.location.href = "/index";
+						return false;
+					}
+				});
+			});
+		},
+		btnLogin: function(e){
+			var modalLogin = $("#modal-login");
+
+			layer.open({
+				type: 1,
+				title: "用户登录",
+				skin: 'layui-layer-rim',
+				area: ['380px', '240px'],
+				content: modalLogin.html()
+			});
+
+			$("#inputLoginAccount").focus();
+
+				// 用户登录表单提交
+				form.on('submit(formUserLogin)', function (data) {
+					var load = layer.load(1);
+					$.ajax({
+						url: "/api/user/login",
+						type: "POST",
+						data: JSON.stringify(data.field),
+						traditional: true,
+						dataType: "json",
+						contentType: "application/json;charset=UTF-8",
+						success: function (data) {
+							layer.close(load);
+							if (data.success) {
+								window.location.href = "/index";
+								return false;
+							} else {
+								layer.msg(data.msg);
+							}
+						}
+					});
+
+					return false;
+				});
+
+				// 用户注册表单提交
+				form.on('submit(formUserRegist)', function (data) {
+					var load = layer.load(1);
+					$.ajax({
+						url: "/api/user/regist",
+						type: "POST",
+						data: JSON.stringify(data.field),
+						traditional: true,
+						dataType: "json",
+						contentType: "application/json;charset=UTF-8",
+						success: function (data) {
+							layer.close(load);
+							if (data.success) {
+								window.location.href = "/index";
+								return false;
+							} else {
+								layer.msg(data.msg);
+							}
+						}
+					});
+
+					return false;
+				});
+		},
+		btnRegist: function(e){
+			var modalRegist = this.$("#modal-regist");
+
+			layer.open({
+				type: 1,
+				title: "用户注册",
+				skin: 'layui-layer-rim',
+				area: ['640px', '400px'],
+				content: modalRegist.html()
+			});
+
+			this.$("#inputRegistAccount").focus();
+		}
+	}
 }
 </script>
 
@@ -15,8 +258,30 @@ export default {
 	font-family: 'Avenir', Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
+}
+
+div.nav-top{
+	width: 100%;
+	height: 60px;
+}
+
+
+/* 导航栏左侧logo */
+div.nav-logo{
+	float: left;
+}
+
+img.nav-logo{
+	width: 200px;
+	height: 48px;
+	/* margin-left: 75px; */
+	line-height: 60px;
+	margin-top: 6px;
+}
+
+
+/* 导航栏靠右菜单 */
+.nav-right{
+	float: right;
 }
 </style>
---
+++ src/components/Index.vue
@@ -0,0 +1,272 @@
+<template>
+	<!-- 主体内容 -->
+	<div class="body-content">
+		<div class="layui-row">
+			<div class="layui-col-md2 layui-col-sm3" pc="">
+
+				<!-- 侧边栏 -->
+				<div class="sidebar">
+					<div class="sidebar-child">
+						<div class="head">热门搜索</div>
+						<ul>
+							<li>
+								<a href="#">C/C++</a>
+							</li>
+							<li>
+								<a href="#">Unity脚本</a>
+							</li>
+							<li>
+								<a href="#">SpringMVC</a>
+							</li>
+							<li>
+								<a href="#">3D动画制作</a>
+							</li>
+							<li>
+								<a href="#">Spring全家桶</a>
+							</li>
+						</ul>
+					</div>
+
+					<div class="sidebar-child">
+						<div class="head">推荐主题</div>
+						<ul>
+							<li>
+								<a href="#">C++ Boost库</a>
+							</li>
+							<li>
+								<a href="#">Java服务端开发</a>
+							</li>
+							<li>
+								<a href="#">Spring全家桶</a>
+							</li>
+							<li>
+								<a href="#">Flask框架</a>
+							</li>
+							<li>
+								<a href="#">Django框架</a>
+							</li>
+							<li>
+								<a href="#">ASP.NET高级应用</a>
+							</li>
+							<li>
+								<a href="#">3D动画制作</a>
+							</li>
+						</ul>
+					</div>
+
+					<div class="sidebar-child">
+						<div class="head">便捷入口</div>
+						<ul>
+							<li>
+								<a href="#">意见反馈</a>
+							</li>
+							<li>
+								<a href="#">友情链接</a>
+							</li>
+							<li>
+								<a href="#">捐赠/Donate</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="layui-col-md9 layui-col-sm9 layui-col-xs12">
+				<div class="main-content">
+					<div class="content-header">最新文章
+						<span>查看更多...</span>
+					</div>
+
+					<div class="content-body">
+						<ul id="ulNewestArticle" class="layui-timeline">
+							
+						</ul>
+					</div>
+
+					<div class="content-header">热门文章
+						<span>查看更多...</span>
+					</div>
+
+					<div class="content-body">
+						<ul class="layui-timeline">
+							<li class="layui-timeline-item">
+								<i class="layui-icon layui-timeline-axis">&#xe63f;</i>
+								<div class="layui-timeline-content layui-text">
+									<h3 class="layui-timeline-title">layui 2.0 的一切准备工作似乎都已到位。发布之弦，一触即发。</h3>
+									<p>
+										<span class="art-span art-author layui-elip">代达罗斯之殇123123123</span>
+										<span class="art-span art-time layui-elip">1秒前</span>
+										<span class="art-span art-label layui-elip" pc="">Unity</span>
+										<span class="art-span art-count">
+											<i class="layui-icon">&#xe6c6;</i> 35 &nbsp;&nbsp;
+											<i class="layui-icon">&#xe611;</i> 11
+										</span>
+									</p>
+								</div>
+							</li>
+							<li class="layui-timeline-item">
+								<i class="layui-icon layui-timeline-axis">&#xe63f;</i>
+								<div class="layui-timeline-content layui-text">
+									<h3 class="layui-timeline-title">杜甫的思想核心是儒家的仁政思想，他有“</h3>
+									<p>
+										<span class="art-span art-author layui-elip">代达罗斯之殇</span>
+										<span class="art-span art-time layui-elip">1秒前</span>
+										<span class="art-span art-label layui-elip" pc="">Unity</span>
+										<span class="art-span art-count">
+											<i class="layui-icon">&#xe6c6;</i> 35 &nbsp;&nbsp;
+											<i class="layui-icon">&#xe611;</i> 11
+										</span>
+									</p>
+								</div>
+							</li>
+							<li class="layui-timeline-item">
+								<i class="layui-icon layui-timeline-axis">&#xe63f;</i>
+								<div class="layui-timeline-content layui-text">
+									<h3 class="layui-timeline-title">8月15日</h3>
+									<p>
+										<span class="art-span art-author layui-elip">代达罗斯之殇</span>
+										<span class="art-span art-time layui-elip">1秒前</span>
+										<span class="art-span art-label layui-elip" pc="">Unity</span>
+										<span class="art-span art-count">
+											<i class="layui-icon">&#xe6c6;</i> 35 &nbsp;&nbsp;
+											<i class="layui-icon">&#xe611;</i> 11
+										</span>
+									</p>
+								</div>
+							</li>
+						</ul>
+					</div>
+
+					<div class="content-header">推荐博主
+						<span>换一换</span>
+					</div>
+
+					<div class="content-body">
+						<div class="layui-row">
+							<div class="layui-col-lg3 layui-col-md3 layui-col-sm3 layui-col-xs6">
+								<div class="user-profile">
+									<img src="images/default_icon.png" alt="头像" />
+									<br/>
+									<br/>
+									<span>代达罗斯之殇</span>
+									<br/>
+									<br/>
+									<button class="layui-btn layui-btn-sm">
+										<i class="layui-icon">&#xe654;</i>关 注
+									</button>
+								</div>
+							</div>
+							<div class="layui-col-lg3 layui-col-md3 layui-col-sm3 layui-col-xs6">
+								<div class="user-profile">
+									<img src="images/i1.jpg" alt="头像" />
+									<br/>
+									<br/>
+									<span>天零萃梦</span>
+									<br/>
+									<br/>
+									<button class="layui-btn layui-btn-sm">
+										<i class="layui-icon">&#xe654;</i>关 注
+									</button>
+								</div>
+							</div>
+							<div class="layui-col-lg3 layui-col-md3 layui-col-sm3 layui-col-xs6">
+								<div class="user-profile">
+									<img src="images/i2.jpg" alt="头像" />
+									<br/>
+									<br/>
+									<span>武穆雪</span>
+									<br/>
+									<br/>
+									<button class="layui-btn layui-btn-sm">
+										<i class="layui-icon">&#xe654;</i>关 注
+									</button>
+								</div>
+							</div>
+							<div class="layui-col-lg3 layui-col-md3 layui-col-sm3 layui-col-xs6">
+								<div class="user-profile">
+									<img src="images/default_icon.png" alt="头像" />
+									<br/>
+									<br/>
+									<span>代达罗斯之殇</span>
+									<br/>
+									<br/>
+									<button class="layui-btn layui-btn-sm">
+										<i class="layui-icon">&#xe654;</i>关 注
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "AdminLogin",
+	data() {
+		return {};
+	},
+	mounted() {
+		var Vue = this;
+
+		layui.use(["element", "form", "layer"], function() {
+			var element = layui.element;
+			var form = layui.form;
+			var layer = layui.layer;
+			var $ = layui.jquery;
+
+			// 管理员登录提交
+			form.on("submit(formAdminLogin)", function(data) {
+				var load = layer.load(1);
+
+				$.ajax({
+					url: Vue.globals.BaseUrl + "/api/user/login",
+					type: "POST",
+					data: JSON.stringify(data.field),
+					traditional: true,
+					dataType: "json",
+					contentType: "application/json;charset=UTF-8",
+					headers: {
+						"Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ3amgxMjMiLCJhdXRocyI6IiIsImlzcyI6Im5vb2J1Zy5jb20iLCJleHAiOjE1MjM0NDgwOTh9.oZWy2TGyLUf4hJyyn_OCWPLOS129r7Z4-e5Nndwd2xc"
+					},
+					success: function(data) {
+						layer.close(load);
+						if (data.success) {
+							this.$router.push("/admin/");
+							return false;
+						} else {
+							layer.msg(data.msg);
+						}
+					}
+				});
+
+				return false;
+			});
+		});
+	}
+};
+</script>
+
+<style scoped>
+body {
+	background-color: lightblue;
+}
+
+.login-box {
+	border-radius: 10px;
+	box-shadow: 10px 10px 5px #0c0c0c;
+	background-color: #2d93ca;
+	padding: 25px 35px;
+}
+
+.layui-form-label {
+	font-size: 16px;
+}
+
+.layui-input-block input {
+	border-radius: 6px;
+}
+</style>
